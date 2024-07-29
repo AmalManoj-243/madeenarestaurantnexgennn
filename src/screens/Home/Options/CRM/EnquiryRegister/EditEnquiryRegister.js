eb71b24016136366a4d3a49b74ab7a989aefcd1f
ingointo <vishnuvishnumohanms@gmail.com>
2024-07-29 14:52:47 +0530

    updated folder changes

---
+++ src/screens/Home/Options/CRM/EnquiryRegister/EditEnquiryRegister.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const EditEnquiryRegister = () => {
+  return (
+    <View>
+      <Text>EditEnquiryRegister</Text>
+    </View>
+  )
+}
+
+export default EditEnquiryRegister
